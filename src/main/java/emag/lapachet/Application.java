e6d01a953d4551ed5756e71e7358b532607a6bf5
Viorel Gheba <viorel.gheba@emag.ro>
2016-09-03 23:07:02 +0300

    Added port

--- src/main/java/emag/lapachet/Application.java
+++ src/main/java/emag/lapachet/Application.java
@@ -10,11 +10,19 @@ import static spark.Spark.*;
 
 public class Application {
 
+    private static final Integer PORT;
+
+    static {
+        PORT = Integer.valueOf(System.getenv("PORT"));
+    }
+
     public static void main(String[] args) {
+        port(PORT);
+        staticFileLocation("/public");
+
         new ApiProducts(new SqlProducts());
 
         get("/", (request, response) -> {
-
             Object attributes = IndexController.serveIndexPage();
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
