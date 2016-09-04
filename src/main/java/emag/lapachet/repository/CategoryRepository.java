2739eaf1dd794b0c280252a9726406f2e3e01557
Alin Ionescu <alin.ionescu@emag.com>
2016-09-04 04:18:10 +0300

    add repo and service

--- src/main/java/emag/lapachet/repository/CategoryRepository.java
+++ src/main/java/emag/lapachet/repository/CategoryRepository.java
@@ -10,6 +10,8 @@ import org.bson.Document;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.mongodb.client.model.Filters.eq;
+
 public class CategoryRepository
 {
     public List<Category> getAllCategories() {
@@ -27,6 +29,32 @@ public class CategoryRepository
                         document.getInteger("status")
                 );
                 categories.add(category);
+            }
+        });
+
+        return categories;
+    }
+
+    public List<Document> getDailyCategories(String date)
+    {
+        List<Document> categories = new ArrayList<>();
+
+        Document dailySale = Db.getMongoDatabase()
+                .getCollection("daily_sale")
+                .find(eq("date", date))
+                .first();
+        Integer dailySaleId = Integer.parseInt(dailySale.get("_id").toString());
+
+        FindIterable<Document> iterable = Db.getMongoDatabase().getCollection("sale_item").find(eq("daily_sale_id", dailySaleId));
+
+        iterable.forEach(new Block<Document>() {
+            @Override
+            public void apply(final Document saleItem) {
+                Document product = Db.getMongoDatabase().getCollection("product").find(eq("_id", saleItem.getInteger("product_id"))).first();
+                Document category = Db.getMongoDatabase().getCollection("category").find(eq("_id", product.getInteger("category_id"))).first();
+                if (!categories.contains(category)) {
+                    categories.add(category);
+                }
             }
         });
 
--- src/main/java/emag/lapachet/service/SqlCategory.java
+++ src/main/java/emag/lapachet/service/SqlCategory.java
@@ -2,12 +2,7 @@ package emag.lapachet.service;
 
 import emag.lapachet.modelInterface.CategoryInterface;
 import emag.lapachet.repository.CategoryRepository;
-import static com.mongodb.client.model.Filters.*;
-import com.mongodb.Block;
-import com.mongodb.client.FindIterable;
-import emag.lapachet.util.Db;
-import org.bson.Document;
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -42,29 +37,8 @@ public class SqlCategory implements CategoryInterface
      * @param date string yyyy-mm-dd
      * @return
      */
-    public List<Document> getDailyCategories(String date)
+    public List getDailyCategories(String date)
     {
-        List<Document> categories = new ArrayList<>();
-
-        Document dailySale = Db.getMongoDatabase()
-            .getCollection("daily_sale")
-            .find(eq("date", date))
-            .first();
-        Integer dailySaleId = Integer.parseInt(dailySale.get("_id").toString());
-
-        FindIterable<Document> iterable = Db.getMongoDatabase().getCollection("sale_item").find(eq("daily_sale_id", dailySaleId));
-
-        iterable.forEach(new Block<Document>() {
-            @Override
-            public void apply(final Document saleItem) {
-                Document product = Db.getMongoDatabase().getCollection("product").find(eq("_id", saleItem.getInteger("product_id"))).first();
-                Document category = Db.getMongoDatabase().getCollection("category").find(eq("_id", product.getInteger("category_id"))).first();
-                if (!categories.contains(category)) {
-                    categories.add(category);
-                }
-            }
-        });
-
-        return categories;
+        return (List) this.categoryRepository.getDailyCategories(date);
     }
 }
